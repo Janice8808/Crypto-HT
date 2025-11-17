826b59b700abfe187ff924531bcc90b7adbc3669
xs <xiaosheng908080@gmail.com>
2025-11-17 14:18:19 -0500

    switch MySQL to PostgreSQL

--- models/db.js
+++ models/db.js
@@ -1,15 +1,10 @@
 // models/db.js
-const mysql = require('mysql2/promise');
-require('dotenv').config();
+const { Pool } = require("pg");
+require("dotenv").config();
 
-const pool = mysql.createPool({
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || 'xs5211314',
-    database: process.env.DB_NAME || 'virtual_exchange',
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false }
 });
 
 module.exports = pool;
